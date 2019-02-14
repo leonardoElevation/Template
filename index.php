87928cc0c8b3dec824dec2c81fa6920416c8d7ee
Leonardo Guerra <leonardo@elevation.com.mx>
2019-02-14 16:22:53 -0600

    Test

--- index.php
+++ index.php
@@ -52,7 +52,6 @@
   </body>
   <script src="js/jquery.min.js"></script>
   <script src="js/popper.min.js"></script>
-  <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js" ></script>
 
   <!--Sweet alert --> 
